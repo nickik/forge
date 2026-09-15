6e9863b4334dff278fc71f9376d262177be19d14
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-15 18:53:28 +0200

    C11c: layer global loads over C9d [skip ci]

---
+++ crates/forge-codegen-cranelift/src/function_c11c_impl.rs
@@ -0,0 +1,8 @@
+mod implementation {
+    include!("function_c9c_impl.rs");
+    include!("function_c9d_extension.rs");
+    include!("function_c11c_extension.rs");
+}
+
+pub(crate) use implementation::lower_function_c9d as lower_function;
+pub(crate) use implementation::lower_function_c11c as lower_function_with_globals;
