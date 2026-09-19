8ed11fc08a458f92b247739259a5e0eb5dd08efa
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-20 01:28:02 +0200

    M28.5: resolve fixed SIA32 GPR ABI builtins

--- crates/forge-frontend/src/body_hir_v1.rs
+++ crates/forge-frontend/src/body_hir_v1.rs
@@ -1471,6 +1471,8 @@ fn resolved_builtin_value(name: &str) -> Option<ResolvedBuiltinValue> {
         "sia_trap" => ResolvedBuiltinValue::SiaTrap,
         "sia_sread" => ResolvedBuiltinValue::SiaSread,
         "sia_swrite" => ResolvedBuiltinValue::SiaSwrite,
+        "sia_gpr_read" => ResolvedBuiltinValue::SiaGprRead,
+        "sia_gpr_write" => ResolvedBuiltinValue::SiaGprWrite,
         "sia_sswap_scratch" => ResolvedBuiltinValue::SiaSswapScratch,
         "sia_sret" => ResolvedBuiltinValue::SiaSret,
         "sia_sretctx" => ResolvedBuiltinValue::SiaSretctx,
